1978e77394ca0433948442e2db0fe1fc687266f0
Eren <erencort1@gmail.com>
2023-07-07 17:16:25 +0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import { useState } from 'react/cjs/react.development';
-import './App.css';
-import { MainContext } from './context';
-import Header from './components/Header';
-import InfoCard from './components/InfoCard';
-import { useEffect } from 'react';
-import axios from 'axios';
+import "./App.css";
+import { MainContext } from "./context";
+import Header from "./components/Header";
+import InfoCard from "./components/InfoCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [search, setSearch] = useState("")
-  const [coinList, setCoinList] = useState([])
-  
+  const [search, setSearch] = useState("");
+  const [coinList, setCoinList] = useState([]);
+
   const data = {
     search,
-    setSearch
-  }
+    setSearch,
+  };
 
   useEffect(() => {
-    fetchreq()
-  }, [])
+    fetchreq();
+  }, []);
 
-  const fetchreq =  async () => {
-    const resp = await axios.get("https://api.coinstats.app/public/v1/coins?skip=0&currency=USD")
-    console.log(resp.data.coins)
-    setCoinList(resp.data.coins)
-  }
+  const fetchreq = async () => {
+    const resp = await axios.get(
+      "https://api.coinstats.app/public/v1/coins?skip=0&currency=USD"
+    );
+    console.log(resp.data.coins);
+    setCoinList(resp.data.coins);
+  };
 
-  const filteredCoinList = coinList.filter(coin =>{
-    return coin.id.includes(search.toLowerCase())
-  })
+  const filteredCoinList = coinList.filter((coin) => {
+    return coin.id.includes(search.toLowerCase());
+  });
 
   return (
-    <MainContext.Provider value={data} >
-      <div className='app'>
+    <MainContext.Provider value={data}>
+      <div className="app">
         <Header />
-        {filteredCoinList.map((coins, index) => 
-        <InfoCard key={index} name={coins.name} symbol={coins.symbol} icon={coins.icon} price={coins.price} />
-        )}
+        {filteredCoinList.map((coins, index) => (
+          <InfoCard
+            key={index}
+            name={coins.name}
+            symbol={coins.symbol}
+            icon={coins.icon}
+            price={coins.price}
+          />
+        ))}
       </div>
     </MainContext.Provider>
   );
